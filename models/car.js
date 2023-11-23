507ab725463edc5580634b2e2cb8341de5182895
AlvinLiem <avh8project2023@gmail.com>
2023-11-23 19:50:50 +0700

    Hanya kurang Invoice

--- models/car.js
+++ models/car.js
@@ -23,15 +23,135 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Car.init({
-    name: DataTypes.STRING,
-    productionYear: DataTypes.STRING,
-    stock: DataTypes.INTEGER,
-    color: DataTypes.STRING,
-    fuelType: DataTypes.STRING,
-    transmission: DataTypes.STRING,
-    price: DataTypes.INTEGER,
-    description: DataTypes.TEXT,
-    carImage: DataTypes.STRING
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: `Name is required`
+        },
+        notEmpty: {
+          msg: `Name is required`
+        }
+      }
+    },
+    productionYear: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: `Production Year is required`
+        },
+        notEmpty: {
+          msg: `Production Year is required`
+        },
+        validYear (value) {
+          let currentYear = new Date().toISOString().slice(0,4)
+          if (value > currentYear) {
+            throw new Error(`Max Year is this year`)
+          } else if (value < (currentYear-15)){
+            throw new Error(`Min Year is 15 years ago`)
+          }
+        }
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: `Stock is required`
+        },
+        notEmpty: {
+          msg: `Stock is required`
+        }
+      }
+    },
+    color: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: `Color is required`
+        },
+        notEmpty: {
+          msg: `Color is required`
+        },
+        validColor (value) {
+          if (value.length > 10) {
+            throw new Error(`Color maximum 10 characters`)
+          }
+        }
+      }
+    },
+    fuelType: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: `Fuel Type is required`
+        },
+        notEmpty: {
+          msg: `Fuel Type is required`
+        }
+      }
+    },
+    transmission: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: `Transmission is required`
+        },
+        notEmpty: {
+          msg: `Transmission is required`
+        }
+      }
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: `Price is required`
+        },
+        notEmpty: {
+          msg: `Price is required`
+        },
+        validPrice (value) {
+          if (value < 100000000) {
+            throw new Error(`Price minimum 100.000.000`)
+          }
+        }
+      }
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: `Description is required`
+        },
+        notEmpty: {
+          msg: `Description is required`
+        }
+      }
+    },
+    carImage: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: `Car Image is required`
+        },
+        notEmpty: {
+          msg: `Car Image is required`
+        },
+        isUrl: {
+          msg: `Car Image must be URL format`
+        }
+      }
+    }
   }, {
     sequelize,
     modelName: 'Car',
